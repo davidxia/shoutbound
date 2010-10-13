194ab8efa07c79172ac5dab463b9b93d00ca8dc5
nan <thenanyu@gmail.com>
2010-10-13 06:53:33 +0000

    ...

--- htdocs/system/application/views/trip.php
+++ htdocs/system/application/views/trip.php
@@ -7,6 +7,9 @@
 <!DOCTYPE html> 
 <html> 
 <head> 
+    
+    <?=$this->load->view('core_header')?>
+    
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" /> 
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
 
