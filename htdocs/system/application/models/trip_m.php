6b755a280e6f78c546d0820e83eb3fc9fdbb8000
Benjamin Sussman <benjamin.sussman@gmail.com>
2010-10-16 16:34:31 -0700

    trip model now can handle suggestions (called items) in the expected ways (create, approve or reject)

--- htdocs/system/application/models/trip_m.php
+++ htdocs/system/application/models/trip_m.php
@@ -12,4 +12,74 @@ class Trip_m extends Model {
         $this->mdb->alter($sql, $values);
     }
 
+
+    function get_user_tripids($uid) {
+        $key = 'tripids_by_uid:'.$uid;
+        $tripids = $this->mc->get($key);
+        if($tripids === false || true) {
+            $sql = 'SELECT tripid FROM trips WHERE uid = ?';
+            $v = array($uid);
+            $rows = $this->mdb->select($sql, $v);
+            $tripids = array();
+            if($rows) {
+                foreach($rows as $row) {
+                    $tripids[] = $row['tripid'];
+                }
+            }
+            $this->mc->set($key, $tripids);
+        }
+        return $tripids;
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////
+    // [Trip] Items (Suggestions)
+
+    function get_item_by_id($itemid) {
+        $key = 'item_by_id:'.$itemid;
+        $val = $this->mc->get($key);
+        if($val === false || true) {
+            $sql = 'SELECT * FROM trip_items WHERE itemid = ?';
+            $v = array($itemid);
+            $rows = $this->mdb->select($sql, $v);
+            $val = $rows[0];
+            $this->mc->set($key, $val);
+        }
+        return $val;
+    }
+
+
+    function add_item($uid, $tripid, $yelpid, $title, $body, $lat, $lon) {
+        $d = array('uid' => $uid,
+                   'tripid' => $tripid,
+                   'yelpid' => $yelpid,
+                   'title' => $title,
+                   'body' => $body,
+                   'lat' => $lat,
+                   'lon' => $lon,
+               );
+        list($sql, $values) = $this->mdb->insert_string('trip_items', $d);
+        $itemid = $this->mdb->alter($sql, $values);
+    }
+
+
+    function update_item($itemid, $status) {
+        $uid = $this->User_m->get_logged_in_uid();
+        $item = $this->get_item_by_id($itemid);
+        if(!$uid || !$item)
+            return false;
+
+        // Users can only update items within THEIR trips
+        $user_trips = $this->get_user_tripids($uid);
+        if(!in_array($item['tripid'], $user_trips)) {
+            return false;
+        }
+
+        $sql = 'UPDATE trip_items SET status = ? WHERE itemid = ?';
+        $v = array($status, $itemid);
+        $this->mdb->alter($sql, $v);
+        return true;
+    }
+
 }
+
