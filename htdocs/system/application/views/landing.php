9a77551f34de885d67d1cf23a235f3255c1a6850
David Xia <david@noqnok.com>
2011-01-17 10:11:27 -0800

    removed ben's nonsense

--- htdocs/system/application/views/landing.php
+++ htdocs/system/application/views/landing.php
@@ -25,8 +25,6 @@
     <img src="<?=site_url('images/fb-login-button.png');?>" />
 </a>
 
-Ben's nonsense
-
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"
         type="text/javascript"></script>
 <script>
