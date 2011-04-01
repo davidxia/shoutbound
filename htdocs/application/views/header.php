c3626f9fb8ec8cab37d58ae1afb5f1f8f00a50d1
David Xia <david@noqnok.com>
2011-04-01 11:25:22 -0400

    modified header to push logo down a bit

--- htdocs/application/views/header.php
+++ htdocs/application/views/header.php
@@ -1,7 +1,7 @@
 <!-- HEADER -->
-<div class="header" style="background-color:#44749D; color:white; font-size:14px; height:50px; display:block;">
+<div class="header" style="font-size:14px; height:60px; display:block;">
   <div class="wrapper" style="width:960px; margin:0 auto; position:relative;">
-    <h1 style="position:absolute; left:0; top:0px;">
+    <h1 style="position:absolute; left:15px; top:10px;">
       <a href="<?=site_url('/')?>"><img src="<?=site_url('images/logo_header.png')?>" alt="Shoutbound" width="81" height="50" style="display:block;"/></a>
     </h1>
     
@@ -11,13 +11,13 @@
     
     <div class="nav" style="position:absolute; right:0; top:0;">
       <? if ($user->id):?>
-        <a href="<?=site_url('home')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:15px 0; color: white; line-height:20px; font-size:14px;">Home</a>
-        <a href="<?=site_url('profile')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:15px 0; color: white; line-height:20px; font-size:14px;">Profile</a>
-        <a href="<?=site_url('settings')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:15px 0; color: white; line-height:20px; font-size:14px;">Settings</a>
-        <a href="<?=site_url('users/logout')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:15px 0; color: white; line-height:20px; font-size:14px;">Logout</a>
+        <a href="<?=site_url('home')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:20px 0; line-height:20px; font-size:14px;">Home</a>
+        <a href="<?=site_url('profile')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:20px 0; line-height:20px; font-size:14px;">Profile</a>
+        <a href="<?=site_url('settings')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:20px 0; line-height:20px; font-size:14px;">Settings</a>
+        <a href="<?=site_url('users/logout')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:20px 0;   line-height:20px; font-size:14px;">Logout</a>
       <? else:?>
-        <a href="<?=site_url('login')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:15px 0; color: white; line-height:20px; font-size:14px;">Login</a>
-        <a href="<?=site_url('signup')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:15px 0; color: white; line-height:20px; font-size:14px;">Sign Up</a>
+        <a href="<?=site_url('login')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:20px 0; line-height:20px; font-size:14px;">Login</a>
+        <a href="<?=site_url('signup')?>" style="margin-left:20px; text-decoration:none; display:block; float:left; padding:20px 0; line-height:20px; font-size:14px;">Sign Up</a>
       <? endif;?>
     </div>
   </div>
