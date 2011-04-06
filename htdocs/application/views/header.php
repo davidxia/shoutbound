f6246bb0908158817a642840389dd7600026b327
David Xia <david@noqnok.com>
2011-04-06 17:18:02 -0400

    bug fixes to header

--- htdocs/application/views/header.php
+++ htdocs/application/views/header.php
@@ -1,15 +1,15 @@
 <!-- HEADER -->
 <div class="header">
   <div class="wrapper">
-    <h1 style="position:absolute;">
+    <h1 style="position:absolute; top:5px; left:0;">
       <a href="<?=site_url('/')?>"><img src="<?=site_url('images/logo_header.png')?>" alt="Shoutbound" width="60" height="35" style="display:block;"/></a>
     </h1>
         
-    <div class="nav" style="float:right;">
-     	<? if ( ! isset($is_landing)):?>
-    		<a href="<?=site_url('trips/create')?>" id="create-trip-button">Create new trip</a>
-    	<? endif;?>
-    	
+   	<? if ( ! isset($is_landing)):?>
+  		<a href="<?=site_url('trips/create')?>" id="create-trip-button" style="position:relative; left:600px; top:3px;">Create new trip</a>
+  	<? endif;?>
+
+    <div class="nav" style="position:absolute; right:0; top:0;">    	
       <? if (isset($user->id)):?>
         <a href="<?=site_url('home')?>">Home</a>
         <a href="<?=site_url('profile')?>">Profile</a>
