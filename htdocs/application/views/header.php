f4acb892d584ea1894c2fd484ac0838a97698e32
James Richards <james@shoutbound.com>
2011-04-06 17:01:45 +0000

    fixed bug in header

--- htdocs/application/views/header.php
+++ htdocs/application/views/header.php
@@ -4,23 +4,13 @@
     <h1 style="position:absolute;">
       <a href="<?=site_url('/')?>"><img src="<?=site_url('images/logo_header.png')?>" alt="Shoutbound" width="60" height="35" style="display:block;"/></a>
     </h1>
-<<<<<<< HEAD
-    
-    <? if ( ! isset($is_landing)):?>
-    	<a href="<?=site_url('trips/create')?>" id="create-trip-button">CREATE A TRIP</a>
-    <? endif;?>
-    
-    <div class="nav" style="position:absolute; right:0; top:0;">
-      <? if (isset($user->id)):?>
-=======
         
     <div class="nav" style="float:right;">
-     	<? if ( ! $is_landing):?>
+     	<? if ( ! isset($is_landing)):?>
     		<a href="<?=site_url('trips/create')?>" id="create-trip-button">Create new trip</a>
     	<? endif;?>
     	
-      <? if ($user->id):?>
->>>>>>> CI2.0
+      <? if (isset($user->id)):?>
         <a href="<?=site_url('home')?>">Home</a>
         <a href="<?=site_url('profile')?>">Profile</a>
         <a href="<?=site_url('settings')?>">Settings</a>
