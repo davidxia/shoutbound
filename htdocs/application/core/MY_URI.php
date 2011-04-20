432eed01fdf6c91d940098220115b6b99a56db9c
David Xia <david@shoutbound.com>
2011-04-20 14:38:32 -0400

    custom handling of disallowed URI chars by extending CI URI core file; users now see our custom 404 page instead of CI 404

---
+++ htdocs/application/core/MY_URI.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_URI extends CI_URI {
+
+	/**
+	 * Filter segments for malicious characters
+	 *
+	 * @access	private
+	 * @param	string
+	 * @return	string
+	 */
+	function _filter_uri($str)
+	{
+		if ($str != '' && $this->config->item('permitted_uri_chars') != '' && $this->config->item('enable_query_strings') == FALSE)
+		{
+			// preg_quote() in PHP 5.3 escapes -, so the str_replace() and addition of - to preg_quote() is to maintain backwards
+			// compatibility as many are unaware of how characters in the permitted_uri_chars will be parsed as a regex pattern
+			if ( ! preg_match("|^[".str_replace(array('\\-', '\-'), '-', preg_quote($this->config->item('permitted_uri_chars'), '-'))."]+$|i", $str))
+			{
+				//show_error('FUUCK.', 400);
+			}
+		}
+
+		// Convert programatic characters to entities
+		$bad	= array('$',		'(',		')',		'%28',		'%29');
+		$good	= array('&#36;',	'&#40;',	'&#41;',	'&#40;',	'&#41;');
+
+		return str_replace($bad, $good, $str);
+	}
+
+    
+}
+
+/* End of file MY_URI.php */
+/* Location: ./application/libraries/MY_URI.php */
