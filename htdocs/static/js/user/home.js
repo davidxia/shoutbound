68bf04365a6949e17b185e497d40532b84d35c5d
David Xia <david@shoutbound.com>
2011-04-25 18:40:25 -0400

    added map js to home page

--- htdocs/static/js/user/home.js
+++ htdocs/static/js/user/home.js
@@ -1,3 +1,36 @@
+var map = {};
+
+map.googleMap;
+
+$(function() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'http://maps.google.com/maps/api/js?sensor=false&callback=map.loadGoogleMap';
+  document.body.appendChild(script);
+});
+
+
+map.loadGoogleMap = function() {
+  var mapOptions = {
+  	disableDefaultUI: true,
+  	mapTypeControl: true,
+  	mapTypeControlOptions: {
+  	  mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE]
+    },
+  	zoomControl: true,
+  	zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.LARGE,
+    },
+  	zoom: 0,
+  	center: new google.maps.LatLng(0,0),
+  	mapTypeId: google.maps.MapTypeId.ROADMAP,    
+    scrollwheel: false
+  };
+  
+  map.googleMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+};
+
+
 $(function() {
   $('#trip-selection').multiselect();
 });
