462b2b6719b8eb3081dd3715bd48d146ce719141
James Richards <james@shoutbound.com>
2011-05-03 13:19:23 -0400

    working on onboarding file

---
+++ htdocs/application/views/onboarding.php
@@ -0,0 +1,24 @@
+<?
+$header_args = array(
+    'title' => 'Onboarding | Shoutbound',
+    'css_paths' => array(
+    ),
+    'js_paths' => array(
+    )
+);
+
+$this->load->view('core_header', $header_args);
+?>
+</head>
+	
+<body>
+  <? $this->load->view('header')?>
+  <? $this->load->view('wrapper_content')?>
+  
+    stuff goes here
+  
+  </div><!-- CONTENT ENDS -->
+  </div><!-- WRAPPER ENDS -->
+  <? $this->load->view('footer')?>
+</body>
+</html>
