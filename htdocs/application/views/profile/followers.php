23f39fb8da360f028fc2f4fe18579dc81073f1ba
James Richards <james@shoutbound.com>
2011-05-03 16:15:49 -0400

    styled followitems on profile page

--- htdocs/application/views/profile/followers.php
+++ htdocs/application/views/profile/followers.php
@@ -1,14 +1,28 @@
 <div id="followers-tab" class="main-tab-content">
   <? if ( ! $profile->followers):?>
-    <?=$profile->name?> doesn't have any followers yet. You can be the <a href="#">first</a>.
+    <?=$profile->name?> doesn't have any followers yet.
   <? endif;?>
+  
   <? foreach ($profile->followers as $follower):?>
-  <div class="follower">
-    <a href="<?=site_url('profile/'.$follower->id)?>"><img src="<?=static_sub('profile_pics/'.$follower->profile_pic)?>" width="50" height="50"/></a>
-    <a href="<?=site_url('profile/'.$follower->id)?>"><?=$follower->name?></a>
-    <? if (isset($follower->place)):?>
-      current location: <a class="place" lat="<?=$follower->place->lat?>" lng="<?=$follower->place->lng?>" href="#"><?=$follower->place->name?></a>
-    <? endif;?>
+  <div class="followitem">
+    <div class="followitem-avatar-container">
+      <a href="<?=site_url('profile/'.$follower->id)?>"><img src="<?=static_sub('profile_pics/'.$follower->profile_pic)?>" width="48" height="48"/></a>
+    </div>
+    
+    <a href="#" class="follow-button">Follow</a>
+    
+    <div class="followitem-content-container">
+      <div class="followitem-title">
+        <a href="<?=site_url('profile/'.$follower->id)?>"><?=$follower->name?></a>
+      </div>
+      <div>Bio here</div>
+      <!--<? if (isset($follower->place)):?>
+        current location: <a class="place" lat="<?=$follower->place->lat?>" lng="<?=$follower->place->lng?>" href="#"><?=$follower->place->name?></a>
+      <? endif;?>-->
+    </div>
+    <div style="clear:both"></div>
   </div>
+  
   <? endforeach;?>
 </div>
+
