004ed8de0991c5ac586c8367f7b6831e1d237914
David Xia <david@shoutbound.com>
2011-05-04 00:42:39 -0400

    bug fix to profile following tab

--- htdocs/application/views/profile/following.php
+++ htdocs/application/views/profile/following.php
@@ -41,12 +41,11 @@
       <a href="<?=site_url('trips/'.$following_trip->id)?>"><?=$following_trip->name?></a>
     </div>
     <? foreach ($following_trip->places as $place):?>
-      <?=$place->name?>
+      <a href="<?=site_url('places/'.$place->id)?>" class="place" lat="<?=$place->lat?>" lng="<?=$place->lng?>"><?=$place->name?></a>
     <? endforeach;?>
     <? foreach ($following_trip->goers as $goer):?>
       <span class="trip-goer">
-        <a href="<?=site_url('profile/'.$goer->id)?>"><img src="<?=static_sub('profile_pics/'.$goer->profile_pic)?>" width="32" height="32"/></a>
-        <a href="<?=site_url('profile/'.$goer->id)?>"><?=$goer->name?></a>
+        <a href="<?=site_url('profile/'.$goer->id)?>"><img src="<?=static_sub('profile_pics/'.$goer->profile_pic)?>" class="tooltip" width="32" height="32" alt="<?=$goer->name?>"/></a>
       </span>
     <? endforeach;?>
     
