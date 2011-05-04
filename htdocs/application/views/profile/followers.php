a97bc215866486e35f37bff21536d498122e30d0
David Xia <david@shoutbound.com>
2011-05-04 00:36:54 -0400

    updating from last conflict

--- htdocs/application/views/profile/followers.php
+++ htdocs/application/views/profile/followers.php
@@ -4,21 +4,25 @@
   <? endif;?>
   
   <? foreach ($profile->followers as $follower):?>
-  <div class="followitem">
+  <div class="followitem" id="user-<?=$follower->id?>">
     <div class="followitem-avatar-container">
       <a href="<?=site_url('profile/'.$follower->id)?>"><img src="<?=static_sub('profile_pics/'.$follower->profile_pic)?>" width="48" height="48"/></a>
     </div>
     
-    <a href="#" class="follow">Follow</a>
+    <? if ($follower->is_following):?>
+      <a href="#" class="unfollow">Unfollow</a>
+    <? elseif ($follower->id != $user->id):?>
+      <a href="#" class="follow">Follow</a>
+    <? endif;?>
     
     <div class="followitem-content-container">
       <div class="followitem-title">
         <a href="<?=site_url('profile/'.$follower->id)?>"><?=$follower->name?></a>
       </div>
-      <div class="followitem-bio"><?=$follower->bio?></div>
-      <!--<? if (isset($follower->place)):?>
+      <div>Bio here</div>
+      <? if (isset($follower->place)):?>
         current location: <a class="place" lat="<?=$follower->place->lat?>" lng="<?=$follower->place->lng?>" href="#"><?=$follower->place->name?></a>
-      <? endif;?>-->
+      <? endif;?>
     </div>
     <div style="clear:both"></div>
   </div>
