5f9ad34e395d35148b642844fc68190ea2bac33d
David Xia <david@shoutbound.com>
2011-05-04 00:55:40 -0400

    updating after resolving conflict

--- htdocs/application/views/profile/following.php
+++ htdocs/application/views/profile/following.php
@@ -31,25 +31,35 @@
     <div style="clear:both"></div> 
        
   </div>
-  
+
   <? endforeach;?>
   
   <? foreach ($profile->following_trips as $following_trip):?>
-  
   <div class="followitem">
     <div class="followitem-title">
       <a href="<?=site_url('trips/'.$following_trip->id)?>"><?=$following_trip->name?></a>
     </div>
     <? foreach ($following_trip->places as $place):?>
-      <?=$place->name?>
+      <a href="<?=site_url('places/'.$place->id)?>" class="place" lat="<?=$place->lat?>" lng="<?=$place->lng?>"><?=$place->name?></a>
     <? endforeach;?>
     <? foreach ($following_trip->goers as $goer):?>
       <span class="trip-goer">
-        <a href="<?=site_url('profile/'.$goer->id)?>"><img src="<?=static_sub('profile_pics/'.$goer->profile_pic)?>" width="32" height="32"/></a>
-        <a href="<?=site_url('profile/'.$goer->id)?>"><?=$goer->name?></a>
+        <a href="<?=site_url('profile/'.$goer->id)?>"><img src="<?=static_sub('profile_pics/'.$goer->profile_pic)?>" class="tooltip" width="32" height="32" alt="<?=$goer->name?>"/></a>
       </span>
     <? endforeach;?>
     
+  </div>
+  <? endforeach;?>
+  
+  
+  <? foreach ($profile->following_places as $place):?>
+  <div class="followitem">
+    <div class="followitem-title">
+      <a href="<?=site_url('places/'.$place->id)?>"><?=$place->name?></a>
+    </div>
+    <? foreach ($place->followers as $follower):?>
+      <a href="<?=site_url('profile/'.$goer->id)?>"><img src="<?=static_sub('profile_pics/'.$goer->profile_pic)?>" class="tooltip" width="32" height="32" alt="<?=$goer->name?>"/></a>
+    <? endforeach;?>
   </div>
   <? endforeach;?>
 </div>
