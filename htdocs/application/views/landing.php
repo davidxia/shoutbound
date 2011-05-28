53d7ecda0289a3579b47e306b4cab0f33d09c4f0
James Richards <james@shoutbound.com>
2011-05-28 04:10:34 -0400

    midway through styling landing page, jquery slideshow plugged in

--- htdocs/application/views/landing.php
+++ htdocs/application/views/landing.php
@@ -1,10 +1,11 @@
 <?
 $header_args = array(
-    'title' => 'Have an adventure | Shoutbound',
+    'title' => 'Have an Adventure | Shoutbound',
     'css_paths'=>array(
       'css/landing.css',
     ),
     'js_paths'=>array(
+      'js/jquery/jqFancyTransitions.1.8.min.js',
     )
 );
 
@@ -24,25 +25,57 @@ $this->load->view('core_header', $header_args);
   <? $this->load->view('templates/content')?>
 
   <div id="landing-page-gallery">
-    <img src="http://i.imgur.com/vpuuY.jpg" width="490" height="375"/>
+    <div id="slideshowHolder">
+      <img src="http://i.imgur.com/vpuuY.jpg" width="490" height="375"/>
+      <img src="https://lh4.googleusercontent.com/_lgMz7QRk66k/TI40jh7fCfI/AAAAAAAAAh8/qk-rneImY4A/s800/3765705795_f124a87d70.jpg" width="490" height="375"/>
+      <img src="http://i.imgur.com/ttCWR.jpg" width="490" height="375"/>
+      <img src="http://i.imgur.com/l9L1Y.jpg" width="490" height="375"/>
+    </div>
   </div>
 
   <div id="landing-page-left">
     <div id="landing-page-title">Discover your next travel adventure.</div>    
     <div id="sign-up-button-container">
-      <a href="<?=site_url('signup/index')?>" id="sign-up">Sign up</a>
+      <a href="<?=site_url('signup/index')?>" id="sign-up">Let's go!</a>
     </div>
   </div>
 
   <div style="clear:both"></div>
   
-  <div id="landing-page-bottom">
-    <div class="landing-page-bottom-item">Find great content related to your dream travel destinations.</div>
-    <div class="landing-page-bottom-item">Get travel advice from people you trust.</div>
-    <div class="landing-page-bottom-item">Collaborate with friends to plan your next trip.</div>  
-    <div class="landing-page-bottom-item last">Share your travel aspirations and experiences.</div>  
+  <div id="landing-page-bottom"><!--BOTTOM-->
+  
+    <!-- 1st Bottom Item-->
+    <div class="landing-page-bottom-item">
+      <div class="bottom-item-title">Find relevant travel content<br/>from people you trust.</div>
+      <div class="bottom-item-img-container"></div>
+      <div class="bottom-item-copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</div>  
+    </div>
+    
+    <!-- 2nd Bottom Item-->
+    <div class="landing-page-bottom-item">
+      <div class="bottom-item-title">Collaborate with friends and family to plan your next trip.</div>
+      <div class="bottom-item-img-container"></div>
+      <div class="bottom-item-copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</div>  
+    </div>
+    
+    <!-- 3rd Bottom Item-->    
+    <div class="landing-page-bottom-item">
+      <div class="bottom-item-title">Share your travel dreams<br/>and experiences.</div>
+      <div class="bottom-item-img-container"></div>
+      <div class="bottom-item-copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</div>        
+    </div>
+    
+    <!-- Last Bottom Item-->    
+    <div class="landing-page-bottom-item last">
+      <div class="bottom-item-title">Drive traffic to your travel blog<br/>or related publication.</div>
+      <div class="bottom-item-img-container">
+        <img alt="Wordpress, Tumblr, Blogger" src="<?=site_url('static/images/blogging-services.jpg')?>"/>
+      </div>           
+      <div class="bottom-item-copy">Use Shoutbound to distribute your travel content and increase traffic to your travel blog or website.</div>  
+    </div> 
+    
     <div style="clear:both"></div>
-  </div>
+  </div><!--BOTTOM END-->
 
 
   </div><!-- WRAPPER ENDS -->
@@ -69,6 +102,9 @@ $this->load->view('core_header', $header_args);
   
 
 <script>
+  $('#slideshowHolder').jqFancyTransitions({ width: 490, height: 375, effect:'wave', delay:3500, navigation: false, links: false });
+  
+
   $.fn.labelFader = function() {
     var f = function() {
       var $this = $(this);
