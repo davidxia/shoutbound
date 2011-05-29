f868a4aae6b396af799e2efcdc7bdf2d5dbb9637
David Xia <david@shoutbound.com>
2011-05-29 18:13:02 -0400

    deleted unnecessary controller

--- htdocs/application/models/upload.php
+++ htdocs/application/models/upload.php
@@ -1,28 +1,49 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Upload extends DataMapper
-{
- 
-    public $has_one = array('user');
-
-    var $validation = array(
-        array(
-            'field' => 'user_id',
-            'label' => 'User',
-            'rules' => array('required')
-        ),
-        array(
-            'field' => 'path',
-            'label' => 'Path',
-            'rules' => array('required')
-        ),
-    );
+class Upload_m extends CI_Model
+{    
 
     function __construct($id = NULL)
     {
-        parent::__construct($id);
+        parent::__construct();
+        if (is_int($id))
+        {
+            $this->get_by_id($id);
+        }
     }
+    
+    
+    public function get_by_id($id)
+    {
+        $key = 'upload_by_upload_id:'.$id;
+        $upload = $this->mc->get($key);
+        if ($upload === FALSE)
+        {
+            $sql = 'SELECT * FROM `uploads` WHERE id = ?';
+            $v = array($id);
+            $rows = $this->mdb->select($sql, $v);
+            $upload = (isset($rows[0])) ? $rows[0] : NULL;
+            $this->mc->set($key, $upload);
+        }
+
+        $this->row2obj($upload);
+    }
+    
+
+
+    public function row2obj($row)
+    {
+        if ( ! is_null($row))
+        {
+            foreach (get_object_vars($this) as $k => $v)
+            {
+                $this->$k = $row->$k;
+            }    
+        }
+    }
+
+
 }
 
-/* End of file upload.php */
-/* Location: ./application/models/upload.php */
+/* End of file upload_m.php */
+/* Location: ./application/models/upload_m.php */
