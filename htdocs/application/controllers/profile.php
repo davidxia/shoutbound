01e2a6daed9236e98c2d69e5cac52b5c5bfed522
David Xia <david@shoutbound.com>
2011-05-31 03:27:51 -0400

    put back profile controller methods

--- htdocs/application/controllers/profile.php
+++ htdocs/application/controllers/profile.php
@@ -304,6 +304,357 @@ class Profile extends CI_Controller
 
         $this->load->view('profile/followers', $data);
     }
+
+
+/*
+    public function ajax_save_profile()
+    {
+        $old_bio = $this->user->bio;
+        $old_url = $this->user->url;
+        $bio = trim($this->input->post('bio'));
+        $url = trim($this->input->post('url'));
+        $curr_place_id = $this->input->post('currPlaceId');
+        $changes_made = FALSE;
+        
+        if ($bio != $old_bio)
+        {
+            $this->user->bio = $bio;
+            $this->load->helper('activity');
+            save_activity($this->user->id, 9, TRUE, NULL, NULL, time()-72);
+            $changes_made = TRUE;
+        }
+        if ($url != $old_url)
+        {
+            $this->user->url = $url;
+            $changes_made = TRUE;
+        }
+        
+        $this->user->get_current_place();
+        if ((!isset($this->user->stored->curr_place->id) AND $curr_place_id) OR
+            (isset($this->user->stored->curr_place->id) AND $curr_place_id AND $this->user->stored->curr_place->id != $curr_place_id))
+        {
+            $p = new Place($curr_place_id);
+            if ($this->user->save($p))
+            {
+                $this->user->set_join_field($p, 'timestamp', time()-72);
+            }
+            
+            $this->load->helper('activity');
+            save_activity($this->user->id, 10, $p->id, NULL, NULL, time()-72);
+            
+            $params = array('setting_id' => 10, 'user' => $this->user);
+            $this->load->library('email_notifs', $params);
+            $this->email_notifs->get_emails();
+            $this->email_notifs->compose_email($this->user, $p->stored);
+            $this->email_notifs->send_email();
+            
+            $changes_made = TRUE;
+        }
+
+        if ($changes_made)
+        {
+            if ($this->user->save())
+            {
+                json_success(array('bio' => $bio, 'url' => $url, 'response' => 'Saved.'));
+            }
+            else
+            {
+                json_error('something broke, tell David to fix');
+            }
+        }
+        else
+        {
+            json_success(array('bio' => $bio, 'url' => $url, 'response' => 'Saved.'));
+        }
+        
+    }
+*/
+
+
+
+/*
+    public function profile_pic_uploadify()
+    {
+        if ( ! empty($_FILES)) {
+            $uid = $this->input->post('uid');
+          	$tempFile = $_FILES['Filedata']['tmp_name'];
+          	list($width, $height, $type, $attr) = getimagesize($_FILES['Filedata']['tmp_name']);
+          	
+          	$targetPath = '/var/www/static/profile_pics/';
+          	$targetFile =  str_replace('//','/',$targetPath) . $uid . '_' . $_FILES['Filedata']['name'];
+    
+        		$u = new User();
+        		$u->get_by_id($uid);
+        		$u->profile_pic = $uid . '_' . $_FILES['Filedata']['name'];
+        		$u->save();
+    
+          	$fileTypes  = str_replace('*.','',$_REQUEST['fileext']);
+          	$fileTypes  = str_replace(';','|',$fileTypes);
+          	$typesArray = split('\|',$fileTypes);
+          	$fileParts  = pathinfo($_FILES['Filedata']['name']);
+          	
+          	if (in_array($fileParts['extension'],$typesArray))
+          	{
+            		// Uncomment the following line if you want to make the directory if it doesn't exist
+            		// mkdir(str_replace('//','/',$targetPath), 0755, true);
+            		
+            		move_uploaded_file($tempFile,$targetFile);
+            		echo str_replace($_SERVER['DOCUMENT_ROOT'],'',$targetFile);
+          	}
+          	else
+          	{
+              echo 'Invalid file type.';
+          	}
+        }
+    }
+*/
+
+
+/*
+    public function ajax_edit_following()
+    {
+        $id = $this->input->post('profileId');
+        $follow = $this->input->post('follow');
+        $u = new User($id);
+        
+        if ($follow AND $u->id != $this->user->id)
+        {
+            $this->user->related_user->where('id', $u->id)->include_join_fields()->get();
+            $new_follow = ($this->user->related_user->join_is_following == '0') ? FALSE : TRUE;
+            if ($u->save($this->user))
+            {
+                $u->set_join_field($this->user, 'is_following', 1);
+                if ($new_follow)
+                {
+                    $this->load->helper('activity');
+                    save_activity($this->user->id, 3, $id, NULL, NULL, time()-72);
+                    save_activity($u->id, 8, $this->user->id, NULL, NULL, time()-72);
+                    
+                    $this->load->library('email_notifs', array('setting_id' => 3, 'profile' => $u));
+                    $this->email_notifs->get_emails();
+                    $this->email_notifs->compose_email($this->user, $u->stored);
+                    $this->email_notifs->send_email();
+                }
+                
+                json_success(array('type' => 'user', 'id' => $id, 'follow' => $follow));
+            }
+            else
+            {
+                json_error('something broken, tell David');
+            }
+        }
+        else
+        {
+            $u->set_join_field($this->user, 'is_following', 0);
+            json_success(array('type' => 'user', 'id' => $id, 'follow' => $follow));
+        }
+    }
+*/
+
+
+/*
+    public function ajax_save_user_places()
+    {
+        if ( !$this->user OR !$this->input->post('placesDates'))
+        {
+            return;
+        }
+
+        $places_dates = $this->input->post('placesDates');
+
+        $p = new Place();
+        foreach ($places_dates as $place_date)
+        {
+            if ($place_date['placeId'])
+            {
+                $p->clear();
+                $p->get_by_id($place_date['placeId']);
+                if ($this->user->save($p))
+                {
+                    $date = date_parse_from_format('m/Y', $place_date['date']);
+                    if (checkdate($date['month'], 1, $date['year']))
+                    {
+                        $this->user->set_join_field($p, 'timestamp', strtotime($date['year'].'-'.$date['month'].'-01'));
+                    }
+                }
+            }
+        }
+        
+        json_success(array('response' => 'saved'));
+    }
+*/
+
+
+    public function history()
+    {
+        $data = array(
+            'user' => $this->user,
+        );
+
+        $this->load->view('profile/history', $data);
+    }
+    
+    
+    public function facebook_geohist()
+    {
+        $this->load->library('facebook');
+        $fbdata = $this->facebook->api('/me?fields=name,location,hometown,education,work');
+        
+        $checkins = $this->facebook->api(array(
+            'method' => 'fql.query',
+            'query' => 'SELECT coords FROM checkin WHERE author_uid = me()'
+        ));
+        //print_r($checkins); echo '<br/><br/>';
+        
+        
+        $places = array();
+        if ($fbdata['location']['name'])
+        {
+            $places[] = $fbdata['location']['name'];
+        }
+        if ($fbdata['hometown']['name'])
+        {
+            $places[] = $fbdata['hometown']['name'];
+        }
+        if ($fbdata['education'])
+        {
+            foreach ($fbdata['education'] as $education)
+            {
+                $places[] = $education['school']['name'];
+            }        
+        }
+        if ($fbdata['work'])
+        {
+            foreach ($fbdata['work'] as $work)
+            {
+                $places[] = $work['location']['name'];
+            }
+        }
+        echo json_encode($places);
+    }
+    
+    
+    public function twitter_geohist()
+    {
+        $this->load->library('twitter');
+        if (is_object($r = $this->twitter->authenticate()))
+        {
+            // TODO: store access token somewhere so we don't have to get new one everytime??
+            $tweets = $this->twitter->get($r->oauth_token, $r->oauth_token_secret, 'http://api.twitter.com/1/statuses/user_timeline.json', array('count'=>'200'));
+            foreach ($tweets as $tweet)
+            {
+                if (isset($tweet->place))
+                {
+                    $geodata = json_encode($tweet->place->bounding_box->coordinates[0]);
+                }
+            }
+        }
+        
+        $data = array(
+            'geodata' => $geodata,
+        );
+        
+        $this->load->view('twitter_geohist', $data);
+        
+    }
+    
+    
+    public function foursquare_geohist()
+    {
+        $this->load->library('epifoursquare');
+        $clientId = 'ALCPEQ3OQEJ2WHTJOAPL0YWWYB4KMFFFVK5WHDMIF0YMROYZ';
+        $clientSecret = 'OZC0TGXEGSSPDBPWTCSKVUOZFF3B1JA1AOR3GF5DXM5AU34R';
+        
+        $fsObj = new Epifoursquare($clientId, $clientSecret);
+        $redirectUrl = 'http://dev.shoutbound.com/david/profile/foursquare_callback';
+        $url = $fsObj->getAuthorizeUrl($redirectUrl);
+        echo "<a href=\"$url\">Click here</a>";
+        echo '<br/><br/>';
+    }
+    
+    
+    public function foursquare_callback()
+    {
+        $this->load->library('epifoursquare');
+        $clientId = 'ALCPEQ3OQEJ2WHTJOAPL0YWWYB4KMFFFVK5WHDMIF0YMROYZ';
+        $clientSecret = 'OZC0TGXEGSSPDBPWTCSKVUOZFF3B1JA1AOR3GF5DXM5AU34R';
+        $redirectUrl = 'http://dev.shoutbound.com/david/profile/foursquare_callback';
+
+        $fsObj = new Epifoursquare($clientId, $clientSecret);
+        // exchange the request token for an access token
+        $token = $fsObj->getAccessToken($_GET['code'], $redirectUrl);
+        //print_r($token).'<br/><br/>';
+        // you can store $token->access_token in your database
+        $fsObj->setAccessToken($token->access_token);
+        /*
+        $res = $fsObj->get('/users/self/checkins');
+        $checkins = $res->response->checkins->items;
+        
+        foreach ($checkins as $checkin)
+        {
+            echo $checkin->venue->name.' '.
+                $checkin->createdAt.' '.
+                $checkin->venue->location->lat.' '.
+                $checkin->venue->location->lng.'<br/><br/>';
+        }
+        */
+        // try to get place data from foursquare
+        $params = array(
+            'll'=>'40.71,-74.01',
+            'query'=>'ace hotel',
+        );
+        
+        $res = $fsObj->get('/venues/search', $params);
+        $places = $res->response->groups[0]->items;
+        foreach ($places as $place)
+        {
+            echo 'name: '.$place->name.', address: ', $place->location->address.', '.
+                $place->location->city.', '.
+                'distance: '.$place->location->distance.'<br/><br/>';
+        }
+    }
+    
+    
+    public function flickr_test()
+    {
+        $params = array(
+            'api_key' => 'd78a7c1d0670efe5f00cbf953251f50f',
+            'secret' => 'd081991f6eb0f0e2',
+            'die_on_error' => 'false'
+        );
+        
+        $this->load->library('phpflickr', $params);
+        
+        $f = new phpFlickr('d78a7c1d0670efe5f00cbf953251f50f');
+        /*
+        $recent = $f->photos_getRecent();
+        //print_r($recent);
+        
+        $photo = $recent['photos']['photo'][0];
+        $owner = $f->people_getInfo($photo['owner']);
+        echo "<a href='http://www.flickr.com/photos/" . $photo['owner'] . "/" . $photo['id'] . "/'>";
+        echo $photo['title'];
+        echo "</a> Owner: ";
+        echo "<a href='http://www.flickr.com/people/" . $photo['owner'] . "/'>";
+        echo $owner['username'];
+        echo "</a><br>";
+        */
+        $args = array(
+            'has_geo' => '1',
+            'lat' => '40.75058',
+            'lon' => '-73.99358000000001',
+            //'is_getty' => '1',
+        );
+        
+        $results = $f->photos_search($args);
+        //print_r($results);
+        foreach ($results['photo'] as $photo)
+        {
+            echo "<a href='http://www.flickr.com/photos/" . $photo['owner'] . "/" . $photo['id'] . "/'>";
+            echo $photo['title'];
+            echo '<br/>';
+        }
+    }
 }
 
 /* End of file profile.php */
